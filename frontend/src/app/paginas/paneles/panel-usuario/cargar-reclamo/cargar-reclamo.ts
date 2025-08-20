601162b8181a73afd027bd23b9afb25b96c0023a
hebecpereyra <hebecpereyra@gmail.com>
2025-08-20 20:09:55 +0000

    feat(formulario cargar_reclamo): se crea la estructura paginas/paneles/panel-usuario/cargar_reclamo.

---
+++ frontend/src/app/paginas/paneles/panel-usuario/cargar-reclamo/cargar-reclamo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cargar-reclamo',
+  imports: [],
+  templateUrl: './cargar-reclamo.html',
+  styleUrl: './cargar-reclamo.css'
+})
+export class CargarReclamo {
+
+}
