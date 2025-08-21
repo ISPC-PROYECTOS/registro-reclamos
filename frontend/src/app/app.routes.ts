6ff6ebfafbeca60d9b7edac86951064aee51cee7
marioed.apodaca <marioed.apodaca@gmail.com>
2025-08-21 21:37:29 +0000

    build: sin footer en app.html

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { InicioSesion } from './autenticacion/inicio-sesion/inicio-sesion';
 import { Pagina404 } from './paginas/pagina404/pagina404';
+import { CargarReclamo } from './paginas/paneles/panel-usuario/cargar-reclamo/cargar-reclamo';
+import { VistaUsuario } from './paginas/paneles/panel-usuario/vista-usuario/vista-usuario';
 
 export const routes: Routes = [
     {path:"", redirectTo:"/inicio-sesion", pathMatch:"full"},
     {path:"inicio-sesion", component:InicioSesion},
+    {path: "cargar-reclamo", component: CargarReclamo},
+    {path: "vista-usuario", component: VistaUsuario},
     {path: '**', component: Pagina404}
 ];
