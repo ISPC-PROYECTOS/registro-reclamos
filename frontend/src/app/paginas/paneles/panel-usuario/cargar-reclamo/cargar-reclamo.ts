e91642248e52a11893ad3a80c9c5c3eaeab64cfe
marioed.apodaca <marioed.apodaca@gmail.com>
2025-08-21 14:17:34 +0000

    build: agregado de componente vista-usuario y su enrutamiento

--- frontend/src/app/paginas/paneles/panel-usuario/cargar-reclamo/cargar-reclamo.ts
+++ frontend/src/app/paginas/paneles/panel-usuario/cargar-reclamo/cargar-reclamo.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cargar-reclamo',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cargar-reclamo.html',
   styleUrl: './cargar-reclamo.css'
 })
