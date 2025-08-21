0d941fb876abc1eebcad21004426dc0344aeef29
hebecpereyra <hebecpereyra@gmail.com>
2025-08-21 23:34:35 +0000

    Feat(registro-reclamo): se prueba poner la ruta en app.routes.ts para visualizar el formulario, no funciono

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { InicioSesion } from './autenticacion/inicio-sesion/inicio-sesion';
 import { Pagina404 } from './paginas/pagina404/pagina404';
+import { CargarReclamo } from './paginas/paneles/panel-usuario/cargar-reclamo/cargar-reclamo';
+
 
 export const routes: Routes = [
     {path:"", redirectTo:"/inicio-sesion", pathMatch:"full"},
     {path:"inicio-sesion", component:InicioSesion},
+    {path:"cargar-reclamo", component:CargarReclamo},
     {path: '**', component: Pagina404}
 ];
