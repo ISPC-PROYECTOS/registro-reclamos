beace89bf1d5ddb9ed0e2bfa330d4eefeda681c8
hebecpereyra <hebecpereyra@gmail.com>
2025-08-21 21:02:20 +0000

    Feat(formulario cargar-reclamo): se construye el html y la configuracion de cargar-reclamo.ts, sin pruebas aun, falta configurar la ruta y hacer correcciones necesarias.

--- frontend/src/app/paginas/paneles/panel-usuario/cargar-reclamo/cargar-reclamo.ts
+++ frontend/src/app/paginas/paneles/panel-usuario/cargar-reclamo/cargar-reclamo.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cargar-reclamo',
-  imports: [],
+  standalone: true, // Esto indica que el componente no pertenece a un NgModule
+  imports: [ReactiveFormsModule],
   templateUrl: './cargar-reclamo.html',
   styleUrl: './cargar-reclamo.css'
 })
 export class CargarReclamo {
+  form!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      descripcion: ['', Validators.required],
+      prioridad: ['', Validators.required]
+    });
+  }
+
+  onEnviar(event: Event) {
+    if (this.form.valid) {
+      console.log('Formulario enviado:', this.form.value);
+    } else {
+      console.log('El formulario no es válido.');
+      this.form.markAllAsTouched();
+    }
+  }
 }
+
+
