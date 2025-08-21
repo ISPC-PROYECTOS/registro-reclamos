d05edc82a46b07406255d7984f16bc4eb0919ca1
vpicco1996 <vpicco1996@gmail.com>
2025-08-21 22:14:54 +0000

    [FEAT] Creación y configuración formulario de registro

--- frontend/src/app/autenticacion/registro/registro.ts
+++ frontend/src/app/autenticacion/registro/registro.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css'
 })
 export class Registro {
+  form!:FormGroup;
+  constructor(private formBuilder: FormBuilder){ 
+    this.form=this.formBuilder.group(
+      {
+      nombre:['',[]],
+      apellido:['',[]],
+      dni:['',[]],
+      email:['',[]],
+      password:['',[]],
+      }
+    )
+  }
 
+  onEnviar(event:Event) 
+  { 
+    console.log(this.form.value) 
+  } 
 }
