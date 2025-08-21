081b99850da4a8be2d4d1c0ba7e1af3261eb3ebd
vpicco1996 <vpicco1996@gmail.com>
2025-08-21 22:24:12 +0000

    [FEAT] Modificación de ruta

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { InicioSesion } from './autenticacion/inicio-sesion/inicio-sesion';
 import { Pagina404 } from './paginas/pagina404/pagina404';
+import { Registro } from './autenticacion/registro/registro';
 
 export const routes: Routes = [
     {path:"", redirectTo:"/inicio-sesion", pathMatch:"full"},
     {path:"inicio-sesion", component:InicioSesion},
+    {path:"registro", component:Registro},
     {path: '**', component: Pagina404}
 ];
