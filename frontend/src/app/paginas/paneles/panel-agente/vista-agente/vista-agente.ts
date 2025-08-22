94480ef2e03dc1b294114f376ba31c11c1e5c6d4
marioed.apodaca <marioed.apodaca@gmail.com>
2025-08-22 02:14:03 +0000

    build: agregado de componente vista-agente y su enrutamiento

---
+++ frontend/src/app/paginas/paneles/panel-agente/vista-agente/vista-agente.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-agente',
+  imports: [],
+  templateUrl: './vista-agente.html',
+  styleUrl: './vista-agente.css'
+})
+export class VistaAgente {
+
+}
