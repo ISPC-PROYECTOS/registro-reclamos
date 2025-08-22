5c68b4376811551e45a41ba75e607f5facabd94e
marioed.apodaca <marioed.apodaca@gmail.com>
2025-08-22 13:13:42 +0000

    [refactor] mejoras en archivos app.router, header y nuevos archivos perfil-user.png y package-lock.json luego del git merge con la dev

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { InicioSesion } from './autenticacion/inicio-sesion/inicio-sesion';
 import { Pagina404 } from './paginas/pagina404/pagina404';
 import { CargarReclamo } from './paginas/paneles/panel-usuario/cargar-reclamo/cargar-reclamo';
 import { VistaUsuario } from './paginas/paneles/panel-usuario/vista-usuario/vista-usuario';
+import { VistaAgente } from './paginas/paneles/panel-agente/vista-agente/vista-agente';
 
 export const routes: Routes = [
     {path:"", redirectTo:"/inicio-sesion", pathMatch:"full"},
     {path:"inicio-sesion", component:InicioSesion},
     {path: "cargar-reclamo", component: CargarReclamo},
     {path: "vista-usuario", component: VistaUsuario},
+    {path: "vista-agente", component: VistaAgente  },
     {path: '**', component: Pagina404}
 ];
