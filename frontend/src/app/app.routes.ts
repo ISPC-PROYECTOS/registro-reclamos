44246072c47d366a15bf4f20d7cd27cfab987fd0
jorgemarchisone <jorgemarchisone@gmail.com>
2025-08-23 02:16:47 +0000

    [FIX] Se corrige rutas, se corrige css y etiquetas bootstrap de sobre-nosotros

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioSesion } from './autenticacion/inicio-sesion/inicio-sesion';
 import { Pagina404 } from './paginas/pagina404/pagina404';
 import { Registro } from './autenticacion/registro/registro';
+import { SobreNosotros } from './paginas/sobre-nosotros/sobre-nosotros';
 
 export const routes: Routes = [
     {path:"", redirectTo:"/inicio-sesion", pathMatch:"full"},
     {path:"inicio-sesion", component:InicioSesion},
     {path:"registro", component:Registro},
+    {path: "sobre-nosotros", component:SobreNosotros},
     {path: '**', component: Pagina404}
 ];
--- frontend/src/app/paginas/sobre-nosotros/sobre-nosotros.ts
+++ frontend/src/app/paginas/sobre-nosotros/sobre-nosotros.ts
@@ -10,7 +10,7 @@ import { RouterLink } from '@angular/router';
 export class SobreNosotros {
   profesionalList: { id: number, name: string; perfil: string, photo: string }[] = [
     { id:1, name: "Hebe Pereyra", perfil:"Desarrolladora", photo:"/Imagenes/foto-PereyraHebe.png"},
-    { id:2, name: "Jorge Marchisone", perfil:"Analista", photo:"/Imagenes/foto-MarchisoneJorge.jpg"}, 
+    { id:2, name: "Jorge Marchisone", perfil:"Desarrolador", photo:"/Imagenes/foto-MarchisoneJorge.jpg"}, 
     { id:3, name: "Victoria Picco", perfil:"Scrum Master", photo:"/Imagenes/foto-PiccoVictoria.png"},
     { id:4, name: "Lautaro Barrera", perfil:"Desarrollador", photo:"/Imagenes/foto-BarreraLautaro.jpg"},
     { id:5, name: "Mariela Suarez", perfil:"Desarrolladora", photo:"/Imagenes/foto-SuarezMariela.png"}
