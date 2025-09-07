320a88b47bd7d9e58ccf2ab915b6591e9266c3ea
hebecpereyra <hebecpereyra@gmail.com>
2025-09-07 14:27:16 +0000

    Feat (servicios): Se hace la primera prueba de crear un servicio

--- frontend/src/app/paginas/sobre-nosotros/sobre-nosotros.ts
+++ frontend/src/app/paginas/sobre-nosotros/sobre-nosotros.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { QuienesSomosService } from '../../services/quienes-somos.service';
 
 @Component({
   selector: 'app-sobre-nosotros',
@@ -7,13 +8,16 @@ import { RouterLink } from '@angular/router';
   templateUrl: './sobre-nosotros.html',
   styleUrl: './sobre-nosotros.css'
 })
-export class SobreNosotros {
-  profesionalList: { id: number, name: string; perfil: string, photo: string, portfolio: string }[] = [
-    { id:1, name: "Hebe Pereyra", perfil:"Desarrolladora", photo:"/Imagenes/foto-PereyraHebe.jpg", portfolio: "portfolios/portfolio-hebe/index.html"},
-    { id:2, name: "Jorge Marchisone", perfil:"Desarrolador", photo:"/Imagenes/foto-MarchisoneJorge.jpg", portfolio: "portfolios/portfolio-jorge/index.html"}, 
-    { id:3, name: "Victoria Picco", perfil:"Scrum Master", photo:"/Imagenes/foto-PiccoVictoria.jpg", portfolio: "portfolios/portfolio-victoria/portafolio.html"},
-    { id:4, name: "Lautaro Barrera", perfil:"Desarrollador", photo:"/Imagenes/foto-BarreraLautaro.jpg", portfolio: "portfolios/portfolio-lautaro/portfolio.html"},
-    { id:5, name: "Mariela Suarez", perfil:"Desarrolladora", photo:"/Imagenes/foto-SuarezMariela.jpg", portfolio: "portfolios/portfolio-mariela/portfolio.html"}
-    ];
+export class SobreNosotros implements OnInit{
+  profesionalList: { id: number, name: string; perfil: string, photo: string, portfolio: string }[] = [];
+  
+  constructor(private quienesSomosService: QuienesSomosService){
+
+  }
+  ngOnInit(): void {
+    this.profesionalList = this.quienesSomosService.obtenerProfecionales();
+    
+    throw new Error('Method not implemented.');
+  }
 }
 
---
+++ frontend/src/app/services/quienes-somos.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuienesSomosService {
+
+  private profesionalList: { id: number, name: string; perfil: string, photo: string, portfolio: string }[] = [
+    { id:1, name: "Hebe Pereyra", perfil:"Desarrolladora", photo:"/Imagenes/foto-PereyraHebe.jpg", portfolio: "portfolios/portfolio-hebe/index.html"},
+    { id:2, name: "Jorge Marchisone", perfil:"Desarrolador", photo:"/Imagenes/foto-MarchisoneJorge.jpg", portfolio: "portfolios/portfolio-jorge/index.html"}, 
+    { id:3, name: "Victoria Picco", perfil:"Scrum Master", photo:"/Imagenes/foto-PiccoVictoria.jpg", portfolio: "portfolios/portfolio-victoria/portafolio.html"},
+    { id:4, name: "Lautaro Barrera", perfil:"Desarrollador", photo:"/Imagenes/foto-BarreraLautaro.jpg", portfolio: "portfolios/portfolio-lautaro/portfolio.html"},
+    { id:5, name: "Mariela Suarez", perfil:"Desarrolladora", photo:"/Imagenes/foto-SuarezMariela.jpg", portfolio: "portfolios/portfolio-mariela/portfolio.html"}
+    ];
+
+  constructor() {  }
+
+  obtenerProfecionales() {
+    return this.profesionalList;
+  }
+  
+}
