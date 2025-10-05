f840b5253dfdd234edccef170678f91fac4bef91
JMarchisone <jorgemarchisone@gmail.com>
2025-10-05 11:43:25 -0300

    [FEAT] Se crean urls

--- backend/autenticacion/urls.py
+++ backend/autenticacion/urls.py
@@ -1,4 +1,8 @@
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('inicio-sesion/', views.InicioSesionView.as_view()),
+    path('verificar-email/', views.VerificarEmailView.as_view()),
+    path('registro/', views.RegistroView.as_view()),
 ]
--- backend/gri/urls.py
+++ backend/gri/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/auth/', include('autenticacion.urls')),
+    path('api/v1/usuario/', include('autenticacion.urls')),
 ]
