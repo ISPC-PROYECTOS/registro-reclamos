d017429a71f11137462f13efdfc9068ed09e7bee
JMarchisone <jorgemarchisone@gmail.com>
2025-10-05 11:24:31 -0300

    [FEAT] Se coloca el modelo en admin.py, se corrige rol del modelo, se crea serializer.py para usuario, y se empieza a trabajar urls

--- backend/autenticacion/models.py
+++ backend/autenticacion/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
-class Roles(models.TextChoices):
-    AGENTE = 'agente', 'Agente'
-    USUARIO = 'usuario', 'Usuario'
-class Usuario(models.Model):
+class Usuarios(models.Model):
     
     id = models.AutoField(primary_key=True, help_text="ID único del usuario.")
     nombre = models.CharField(max_length=30, null=False, help_text="Nombre del usuario.")
@@ -12,7 +9,7 @@ class Usuario(models.Model):
     area = models.CharField(max_length=10, null=False, help_text="Área del usuario.")
     email = models.EmailField(unique=True, null=False, validators=[RegexValidator(r'^\d{8,100}$')], help_text="Correo electrónico único.")
     password = models.CharField(max_length=255, null=False, help_text="Contraseña del usuario.")
-    rol = models.CharField(max_length=10, choices=Roles.choices, null=False, default=Roles.USUARIO, help_text="Rol del usuario.")
+    rol = models.CharField(max_length=10, choices=[('agente', 'Agente'), ('usuario', 'Usuario')], null=False, default='usuario', help_text="Rol del usuario.")
     
     def __str__(self):
         return f"{self.nombre} {self.apellido} - {self.email}"
