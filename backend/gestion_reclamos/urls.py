4842a78c2c76e97f7bf5c09db5ac8372d59a101d
PereyraHebe <p_hebe_c@hotmail.com>
2025-10-10 08:22:59 -0300

    Feat: se agrega codigo corregido en modulo gestion_reclamos

---
+++ backend/gestion_reclamos/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ReclamoListCreateView, ReclamoRetrieveUpdateDestroyView
+
+urlpatterns = [
+    # GET y POST a /api/v1/reclamos/
+    path('', ReclamoListCreateView.as_view(), name='reclamo-list-create'),
+
+    # GET, PATCH y DELETE a /api/v1/reclamos/<id>/
+    path('<int:pk>/', ReclamoRetrieveUpdateDestroyView.as_view(), name='reclamo-detail'),
+]
