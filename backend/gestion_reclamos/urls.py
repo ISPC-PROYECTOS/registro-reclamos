b58d202981aa785fbbc85dd788d42b94fb82ec9b
MarielarosaSZ <mariela_suarezz@hotmail.com>
2025-10-14 21:24:02 -0300

    Guardando cambios locales antes de fusionar con la rama más actualizada

--- backend/gestion_reclamos/urls.py
+++ backend/gestion_reclamos/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import ReclamoListCreateView, ReclamoRetrieveUpdateDestroyView
 
 urlpatterns = [
+    # GET y POST a /api/v1/reclamos/
     path('', ReclamoListCreateView.as_view(), name='reclamo-list-create'),
+    # GET, PUT, DELETE a /api/v1/reclamos/<id>/
     path('<int:pk>/', ReclamoRetrieveUpdateDestroyView.as_view(), name='reclamo-detail'),
-    
+]
--- backend/gestion_reclamos/views.py
+++ backend/gestion_reclamos/views.py
@@ -1,15 +1,25 @@
 from rest_framework import generics
 from .models import Reclamo
-from .serializers import ReclamoSerializer 
+from .serializers import ReclamoUserSerializer, ReclamoGestorSerializer
 from rest_framework.permissions import AllowAny
 
 class ReclamoListCreateView(generics.ListCreateAPIView):
     queryset = Reclamo.objects.all()
-    serializer_class = ReclamoSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [AllowAny]  # Permitir acceso sin autenticación
+
+    def get_serializer_class(self):
+        
+        return ReclamoUserSerializer  # Usar el serializador para listado
 
 class ReclamoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Reclamo.objects.all()
-    serializer_class = ReclamoSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [AllowAny] 
+
+    def get_serializer_class(self):
+
+        if self.request.method in ['PATCH', 'PUT']:
+        
+            return ReclamoGestorSerializer  #Sólo permite editar el campo 'estado'
+        
+        return ReclamoUserSerializer  # Para GET (detalle), listamos todos los campos
     
